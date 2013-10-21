a2ee2f504e46d7d7ce6f2730976f754f7a2a4f8f
McCouman <support@wikibyte.org>
2013-10-21 13:03:59 +0200

    2010 - Pegasus Skin
    MediaWiki Design 1.17.x

---
+++ pegasus/images/index.php
@@ -0,0 +1,35 @@
+<html>
+
+<head>
+<meta http-equiv="Content-Language" content="de">
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<meta name="generator" content="Bluefish 2.0.0-rc3" >
+<meta name="ProgId" content="FrontPage.Editor.Document">
+<title>Sehr geehrter Besucher</title>
+</head>
+
+<body>
+
+<p><font size="2" face="Arial, Helvetica, sans-serif"><img border="0" src="http://wikinode.square7.ch/images/403.gif" alt="403 - Zugriff verweigert" width="198" height="57"></font></p>
+<p><font color="#B5D6B1" face="Arial, Helvetica, sans-serif" size="2">_________________________________________________________</font><font face="Arial, Helvetica, sans-serif" size="2">&nbsp;<br>
+  </font></p>
+<table border="0" cellspacing="0" width="515">
+  <tr> 
+    <td width="511"> 
+      <p><font face="Arial, Helvetica, sans-serif" size="2">Sehr geehrter Besucher,</font></p>
+      <p><font face="Arial, Helvetica, sans-serif" size="2">leider ist ein Fehler 
+        aufgetreten. Der Zugriff auf die angeforderte Datei wurde verweigert.</font></p>
+      <p><font face="Arial, Helvetica, sans-serif" size="2">Haben Sie sich vielleicht 
+        vertippt oder eine alte URL aufgerufen? Wenn nicht, informieren Sie bitte 
+        den Webmaster dieser Homepage per Email. Um zu der vorherigen Seite zur&uuml;ck 
+        zu kehren, verwenden Sie bitte einfach die &quot;Zur&uuml;ck&quot; - Taste 
+        Ihres Browsers. </font></p>
+      <p align="right">&nbsp;
+    </td>
+  </tr>
+</table>
+<p align="left"><font color="#B5D6B1" face="Arial, Helvetica, sans-serif" size="2">_________________________________________________________</font></p>
+
+</body>
+
+</html>
